c7c8a0e1f5354c5df75ccfa61e295120d19d7229
zahrasyifaannisa <zahra.gugum@gmail.com>
2021-12-06 00:27:10 +0700

    pertemuan10

---
+++ pertemuan10.py
@@ -0,0 +1,11 @@
+a={"ari":"081267888","dina":"087677776" }
+print(a["ari"])
+a["riko"]= "087654544"
+print(a)
+a["dina"]= "088999776"
+print(a)
+print(a.keys())
+print(a.values())
+print(a)
+del a["dina"]
+print(a)
